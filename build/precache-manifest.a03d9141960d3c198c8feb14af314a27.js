3a05b18de62bb471f31135507341744b62264ee2
Bew <Bew@sysops-MBP.broadband>
2020-10-12 23:17:43 +0100

    React with MongoDB first commit

---
+++ build/precache-manifest.a03d9141960d3c198c8feb14af314a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8521adf3fc29695f804bf2d86c8fb28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f47deb7767bb4df2d8af",
+    "url": "/static/css/main.073ad5f3.chunk.css"
+  },
+  {
+    "revision": "afe758643915ea6f1178",
+    "url": "/static/js/2.a301f395.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a301f395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47deb7767bb4df2d8af",
+    "url": "/static/js/main.9f6af749.chunk.js"
+  },
+  {
+    "revision": "95ebf181ce4f02370162",
+    "url": "/static/js/runtime-main.3b90e43a.js"
+  }
+]);
